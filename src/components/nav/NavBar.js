656d8bb3933a83f35002c847b059730b264c31f5
Tiffani Hess <hesstjune@gmail.com>
2021-04-29 17:20:11 -0500

    link logo button to home

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Image, Button } from "semantic-ui-react";
 
 const NavBar = () => {
@@ -14,22 +15,24 @@ const NavBar = () => {
       }}
     >
       <div style={{ marginRight: "auto" }}>
-        <Button
-          as="a"
-          size="medium"
-          color="#2f528b"
-          title="home"
-          style={{
-            borderRadius: "50%",
-            padding: "8px 7px",
-            boxShadow: "0 0 10px 2px rgba(17,47,167,0.3)",
-            border: "2px solid #2f528b",
-            background:
-              "linear-gradient(135deg, rgba(255,255,255,1) 0%,rgba(246,246,246,1) 47%,rgba(237,237,237,1) 100%)",
-          }}
-        >
-          <Image style={{ height: "2.45rem" }} src="/4flogo.png" circular />
-        </Button>
+        <Link to="/home">
+          <Button
+            as="a"
+            size="medium"
+            color="#2f528b"
+            title="home"
+            style={{
+              borderRadius: "50%",
+              padding: "8px 7px",
+              boxShadow: "0 0 10px 2px rgba(17,47,167,0.3)",
+              border: "2px solid #2f528b",
+              background:
+                "linear-gradient(135deg, rgba(255,255,255,1) 0%,rgba(246,246,246,1) 47%,rgba(237,237,237,1) 100%)",
+            }}
+          >
+            <Image style={{ height: "2.45rem" }} src="/4flogo.png" circular />
+          </Button>
+        </Link>
       </div>
 
       <div style={{ marginLeft: "auto" }}>
