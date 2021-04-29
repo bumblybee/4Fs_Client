3bd48e6b38f9f533dca17bcb2952e8b06d928ee9
Tiffani Hess <hesstjune@gmail.com>
2021-04-29 17:13:03 -0500

    Defined tabs object and function to render tabs, switch statement to render for each path case

--- src/hooks/useTabs.js
+++ src/hooks/useTabs.js
@@ -5,20 +5,40 @@ import { Link } from "react-router-dom";
 const useTabs = (path) => {
   const [activeItem, setActiveItem] = useState("milestones");
 
+  const tabs = {
+    home: ["milestones", "beliefs", "calculator"],
+    focus: ["system", "swagger", "sleep", "habits"],
+    fasting: ["window", "fasting guide"],
+    food: ["calories", "food list", "meal planner"],
+    fitness: ["fitness activities", "fitness planner"],
+  };
+
+  const renderTabs = (tabArr) => {
+    return tabArr.map((tab) => (
+      <Menu.Item
+        as={Link}
+        to={`/${path}/${tab}`}
+        name={tab}
+        active={activeItem === tab}
+        onClick={() => {
+          setActiveItem(tab);
+        }}
+      />
+    ));
+  };
+
   switch (path) {
     case "home":
-      const tabs = ["milestones", "beliefs", "calculator"];
-      return tabs.map((tab) => (
-        <Menu.Item
-          as={Link}
-          to={`/${path}/${tab}`}
-          name={tab}
-          active={activeItem === tab}
-          onClick={() => {
-            setActiveItem(tab);
-          }}
-        />
-      ));
+      return renderTabs(tabs.home);
+    case "focus":
+      return renderTabs(tabs.focus);
+    case "fasting":
+      return renderTabs(tabs.fasting);
+    case "food":
+      return renderTabs(tabs.food);
+    case "fitness":
+      return renderTabs(tabs.fitness);
+
     default:
       return "";
   }
