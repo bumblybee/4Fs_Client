a4ffa09957801ada44718f0920c60ee7d0c969b8
Tiffani Hess <hesstjune@gmail.com>
2021-04-29 10:33:31 -0500

    brought in SectionTabMenu and FTabBar

---
+++ src/components/sectionTabMenu/SectionTabMenu.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Milestones from "../milestones/Milestones";
+import Beliefs from "../beliefs/Beliefs";
+
+import { Container, Menu, Tab } from "semantic-ui-react";
+
+// const panes = [
+//   {
+//     menuItem: "Milestones",
+//     render: () => (
+//       <Tab.Pane attached={false}>
+//         <Milestones />
+//       </Tab.Pane>
+//     ),
+//   },
+//   {
+//     menuItem: "Beliefs",
+//     render: () => (
+//       <Tab.Pane attached={false}>
+//         <Beliefs />
+//       </Tab.Pane>
+//     ),
+//   },
+//   {
+//     menuItem: "Tab 3",
+//     render: () => <Tab.Pane attached={false}>Tab 3 Content</Tab.Pane>,
+//   },
+// ];
+
+const SectionTabMenu = ({ setChild }) => {
+  const [activeItem, setActiveItem] = useState("milestones");
+
+  return (
+    <Container style={{ margin: "2rem auto 3.5rem" }}>
+      <Menu
+        pointing
+        secondary
+        color="blue"
+        size="huge"
+        style={{ width: "min-content", margin: "0 auto 3rem" }}
+      >
+        <Menu.Item
+          name="milestones"
+          active={activeItem === "milestones"}
+          onClick={() => {
+            setActiveItem("milestones");
+            setChild("milestones");
+          }}
+        />
+        <Menu.Item
+          name="beliefs"
+          active={activeItem === "beliefs"}
+          onClick={() => {
+            setActiveItem("beliefs");
+            setChild("beliefs");
+          }}
+        />
+        <Menu.Item
+          name="calculator"
+          active={activeItem === "calculator"}
+          onClick={() => {
+            setActiveItem("calculator");
+            setChild("calculator");
+          }}
+        />
+      </Menu>
+    </Container>
+  );
+};
+
+export default SectionTabMenu;
--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../../components/nav/NavBar";
 import FTabBar from "../../components/fTabBar/FTabBar";
+import SectionTabContainer from "../../components/sectionTabMenu/SectionTabContainer";
 
 const LandingPage = () => {
   return (
     <div>
       <NavBar />
       <FTabBar />
+      <SectionTabContainer />
     </div>
   );
 };
