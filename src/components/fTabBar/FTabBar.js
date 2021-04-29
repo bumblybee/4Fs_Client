2a3045c4a38bb5879b08a5201e048717694ef521
Tiffani Hess <hesstjune@gmail.com>
2021-04-29 17:18:57 -0500

    Wrapped FBar items in Links, styled so all match

--- src/components/fTabBar/FTabBar.js
+++ src/components/fTabBar/FTabBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import { Menu, Icon } from "semantic-ui-react";
 
@@ -29,76 +30,87 @@ const FTabBar = () => {
           margin: "0",
         }}
       >
-        <Menu.Item
-          name="focus"
-          active={activeItem === "focus"}
-          style={{
-            background: activeItem === "focus" ? "#2b73ce" : "",
-            color: activeItem === "focus" ? "hsl(217, 49%, 99%)" : "#2b73ce",
-            padding: "1.25rem 1.75rem",
-            borderRadius: 0,
-            fontFamily: "Montserrat",
-            fontWeight: 400,
-          }}
-          onClick={(e) => setActiveItem("focus")}
-        >
-          <Icon name="bullseye" size="large" />
-
-          <span
+        <Link to="/focus">
+          <Menu.Item
+            name="focus"
+            active={activeItem === "focus"}
             style={{
-              // fontSize: "0.925rem",
-              // textAlign: "center",
-              // lineHeight: 1.1,
-              fontWeight: 500,
+              background: activeItem === "focus" ? "#2b73ce" : "",
+              color: activeItem === "focus" ? "hsl(217, 49%, 99%)" : "#2b73ce",
+              padding: "1.25rem 1.75rem",
+              borderRadius: 0,
+              fontFamily: "Montserrat",
+              fontWeight: 400,
             }}
+            onClick={(e) => setActiveItem("focus")}
           >
-            Focus
-          </span>
-        </Menu.Item>
-        <Menu.Item
-          style={{
-            background: activeItem === "fasting" ? "#2b73ce" : "",
-            color: activeItem === "fasting" ? "hsl(217, 49%, 99%)" : "#2b73ce",
-            borderLeft: "1px solid #2b73ce",
-            fontFamily: "Montserrat",
-            fontWeight: 500,
-          }}
-          name="fasting"
-          onClick={(e) => setActiveItem("fasting")}
-        >
-          <Icon name="wait" size="large" />
-          Fasting
-        </Menu.Item>
-        <Menu.Item
-          style={{
-            background: activeItem === "food" ? "#2b73ce" : "",
-            color: activeItem === "food" ? "hsl(217, 49%, 99%)" : "#2b73ce",
-            borderLeft: "1px solid #2b73ce",
-            fontFamily: "Montserrat",
-            fontWeight: 500,
-          }}
-          onClick={(e) => setActiveItem("food")}
-          name="food"
-        >
-          <Icon name="food" size="large" />
-          Food
-        </Menu.Item>
-        <Menu.Item
-          style={{
-            background: activeItem === "fitness" ? "#2b73ce" : "",
-            color: activeItem === "fitness" ? "hsl(217, 49%, 99%)" : "#2b73ce",
-            borderLeft: "1px solid #2b73ce",
-            fontFamily: "Montserrat",
-            fontWeight: 500,
-          }}
-          onClick={(e) => setActiveItem("fitness")}
-          name="fitness"
-        >
-          <Icon name="heartbeat" size="large" />
-          Fitness
-        </Menu.Item>
+            <Icon name="bullseye" size="large" />
+
+            <span
+              style={{
+                fontWeight: 500,
+              }}
+            >
+              Focus
+            </span>
+          </Menu.Item>
+        </Link>
+        <Link to="/fasting">
+          <Menu.Item
+            style={{
+              background: activeItem === "fasting" ? "#2b73ce" : "",
+              color:
+                activeItem === "fasting" ? "hsl(217, 49%, 99%)" : "#2b73ce",
+              padding: "1.25rem 1.75rem",
+              borderRadius: 0,
+              fontFamily: "Montserrat",
+              fontWeight: 400,
+            }}
+            name="fasting"
+            onClick={(e) => setActiveItem("fasting")}
+          >
+            <Icon name="wait" size="large" />
+            Fasting
+          </Menu.Item>
+        </Link>
+        <Link to="/food">
+          <Menu.Item
+            style={{
+              background: activeItem === "food" ? "#2b73ce" : "",
+              color: activeItem === "food" ? "hsl(217, 49%, 99%)" : "#2b73ce",
+              padding: "1.25rem 1.75rem",
+              borderRadius: 0,
+              fontFamily: "Montserrat",
+              fontWeight: 400,
+            }}
+            onClick={(e) => setActiveItem("food")}
+            name="food"
+          >
+            <Icon name="food" size="large" />
+            Food
+          </Menu.Item>
+        </Link>
+        <Link to="/fitness">
+          <Menu.Item
+            style={{
+              background: activeItem === "fitness" ? "#2b73ce" : "",
+              color:
+                activeItem === "fitness" ? "hsl(217, 49%, 99%)" : "#2b73ce",
+              padding: "1.25rem 1.75rem",
+              borderRadius: 0,
+              fontFamily: "Montserrat",
+              fontWeight: 400,
+            }}
+            onClick={(e) => setActiveItem("fitness")}
+            name="fitness"
+          >
+            <Icon name="heartbeat" size="large" />
+            Fitness
+          </Menu.Item>
+        </Link>
       </Menu>
-      <a
+      <Link
+        to="/resources"
         style={{
           position: "absolute",
           top: "40%",
@@ -109,7 +121,7 @@ const FTabBar = () => {
         }}
       >
         Bonus Resources
-      </a>
+      </Link>
     </div>
   );
 };
