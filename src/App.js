43c825239075f5f1e98bc97c99281aa7377276e3
Tiffani Hess <hesstjune@gmail.com>
2021-04-29 15:16:32 -0500

    added routes in App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LandingPage from "./pages/landing/LandingPage";
+
 import {
   Route,
   BrowserRouter as Router,
@@ -14,8 +14,35 @@ function App() {
     <Router>
       <Switch>
         <div className="App">
-          <Route path="/home">
-            <LandingPage />
+          <NavBar />
+          <FBar />
+          <TabMenu />
+          <Route path="/home" exact>
+            <Redirect to="/home/milestones" />
+          </Route>
+          <Route path="/home/milestones">
+            <TableContainer>
+              <Milestones />
+            </TableContainer>
+          </Route>
+          <Route path="/focus" exact>
+            <Redirect to="/focus/system" />
+          </Route>
+          <Route path="/focus/system">
+            <System />
+          </Route>
+          <Route path="/focus/swagger"></Route>
+          <Route path="/focus/sleep"></Route>
+          <Route path="/focus/habits"></Route>
+
+          <Route path="/fasting">
+            <Redirect to="/fasting/window" />
+          </Route>
+          <Route path="/food">
+            <Redirect to="/food/calories" />
+          </Route>
+          <Route path="/fitness">
+            <Redirect to="/fitness/activities" />
           </Route>
           <Route path="/login">
             <LoginForm />
@@ -24,7 +51,7 @@ function App() {
             <SignupForm />
           </Route>
           <Route path="/" exact>
-            <Redirect to="/signup" />
+            <Redirect to="/home" />
           </Route>
         </div>
       </Switch>
