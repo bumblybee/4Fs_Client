6175193d2a8302b0099b453cf83bcb741a704763
Tiffani Hess <hesstjune@gmail.com>
2021-04-29 10:42:49 -0500

    Created TableComponent

---
+++ src/table/StyledTable.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { Table } from "semantic-ui-react";
+
+export const StyledTable = styled(Table)`
+  tbody {
+    display: block;
+    height: ${(props) => (props.rowdata.length > 9 ? "33.5rem" : "auto")};
+    overflow: auto;
+  }
+
+  thead,
+  tbody tr {
+    display: table;
+    width: 100%;
+    table-layout: fixed;
+  }
+
+  input[type="text"] {
+    border: none;
+
+    background: #eeeeee55;
+    :focus {
+      outline: none;
+      border: none;
+      background: #eee;
+    }
+  }
+
+  input[type="date"] {
+    :hover {
+      cursor: pointer;
+    }
+  }
+
+  /* Responsive table */
+
+  @media only screen and (max-width: 760px),
+    (min-device-width: 768px) and (max-device-width: 1024px) {
+    /* Force table to not be like tables anymore */
+    table,
+    thead,
+    tbody,
+    th,
+    td,
+    tr {
+      display: block;
+    }
+
+    tr {
+      border-bottom-width: 2px;
+      border-bottom-style: solid;
+      border-bottom-color: ${(props) => props.color};
+    }
+
+    /* Hide table headers (but not display: none;, for accessibility) */
+    thead tr {
+      position: absolute;
+      top: -9999px;
+      left: -9999px;
+    }
+
+    td {
+      /* Behave  like a "row" */
+      border: none;
+      border-bottom: 1px solid #eee;
+      position: relative;
+      padding-left: 50%;
+    }
+
+    td:before {
+      /* Now like a table header */
+      position: absolute;
+      /* Top/left values mimic padding */
+      top: 6px;
+      left: 6px;
+      width: 45%;
+      padding-right: 10px;
+      white-space: nowrap;
+    }
+  }
+`;
---
+++ src/table/TableComponent.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { Table } from "semantic-ui-react";
+import { StyledTable } from "./StyledTable";
+
+TableComponent.TD = function TD({ children, ...props }) {
+  return (
+    <Table.Cell {...props} style={{ minHeight: "45px" }}>
+      {children}
+    </Table.Cell>
+  );
+};
+
+TableComponent.TR = function TR({ children, ...props }) {
+  return <Table.Row {...props}>{children}</Table.Row>;
+};
+
+TableComponent.TH = function TH({ children, ...props }) {
+  return (
+    <Table.HeaderCell width={props.columnwidth} {...props}>
+      {children}
+    </Table.HeaderCell>
+  );
+};
+
+TableComponent.TF = function TF({ children, ...props }) {
+  return <Table.Footer {...props}>{children}</Table.Footer>;
+};
+
+const TableComponent = ({ children, ...props }) => {
+  const [rowData, setRowData] = useState(props.rows);
+
+  useEffect(() => {
+    setRowData([...props.rows]);
+  }, [props.rows]);
+
+  const renderColumns = (columns) => {
+    return columns.map((header, idx) => (
+      <TableComponent.TH
+        key={idx}
+        style={{
+          backgroundColor: props.color,
+          color: "#fff",
+          borderRadius: props.descriptionheader ? "0" : "",
+        }}
+      >
+        {header.label}
+      </TableComponent.TH>
+    ));
+  };
+
+  const renderDescriptionHeader = () => {
+    /* If we have an additional header with descriptive text */
+    return (
+      <Table.Header>
+        <TableComponent.TR>
+          <TableComponent.TH
+            style={{
+              backgroundColor: props.color,
+              color: "#fff",
+              textAlign: "center",
+            }}
+          >
+            {props.descriptionheader}
+          </TableComponent.TH>
+        </TableComponent.TR>
+      </Table.Header>
+    );
+  };
+
+  const rowsMappedToColumns = rowData.map((row) => {
+    const rowCells = props.columns.map((column) => {
+      const { key: columnKey } = column;
+
+      return row[columnKey].cellComponent;
+    });
+    return rowCells;
+  });
+
+  function renderRows(mappedRows) {
+    return mappedRows.map((row, idx) => (
+      <TableComponent.TR
+        key={idx}
+        style={{
+          minHeight: "40px",
+          textAlign: props.aligntext,
+        }}
+      >
+        {renderCells(row)}
+      </TableComponent.TR>
+    ));
+  }
+
+  const renderCells = (row) => {
+    return row.map((cellComponent, idx) => (
+      <TableComponent.TD key={idx}>{cellComponent}</TableComponent.TD>
+    ));
+  };
+
+  return (
+    <StyledTable
+      {...props}
+      rowdata={rowData}
+      color={props.color}
+      compact
+      unstackable
+      striped={props.striped}
+      collapsing={props.collapsing}
+    >
+      {props.descriptionheader && renderDescriptionHeader()}
+
+      <Table.Header>
+        <TableComponent.TR style={{ textAlign: props.aligntext }}>
+          {renderColumns(props.columns)}
+        </TableComponent.TR>
+      </Table.Header>
+      <Table.Body>{renderRows(rowsMappedToColumns)}</Table.Body>
+    </StyledTable>
+  );
+};
+
+export default TableComponent;
