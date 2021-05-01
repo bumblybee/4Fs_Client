a0e8130d72e59115b47ae31ce24b6c4da5f3f4ad
Tiffani Hess <hesstjune@gmail.com>
2021-05-01 13:18:32 -0500

    Message to show error in Error

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,16 +1,20 @@
 import React, { useContext } from "react";
 import { ErrorContext } from "../../context/error/ErrorContext";
+import { Message } from "semantic-ui-react";
 // import { StyledNotificationPopup } from "../../styles/GlobalStyledComponents";
 
 const Error = () => {
   const { errorMessage } = useContext(ErrorContext);
+  const title = errorMessage.split(".")[0];
+  const body = errorMessage.split(".")[1];
 
   return (
-    // errorMessage !== null && (
-    //   <StyledNotificationPopup origin={"error"} draggable>
-    //     {errorMessage}
-    //   </StyledNotificationPopup>
-    // )
+    errorMessage !== null && (
+      <Message color="red">
+        <Message.Header>{title}</Message.Header>
+        <p>{body}</p>
+      </Message>
+    )
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-less/semantic.less";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
