5a9f4ff7f6231f7132c1ea005aade7b9dbbc9436
Tiffani Hess <hesstjune@gmail.com>
2021-05-01 13:08:17 -0500

    Defined Error component and brought into App

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
+
+import ErrorProvider from "./context/error/ErrorProvider";
 import UserProvider from "./context/user/UserProvider";
+
 import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
 import AppWrapper from "./components/wrapper/AppWrapper";
 import SignupForm from "./pages/signup/SignupForm";
@@ -17,56 +20,58 @@ import System from "./components/system/System";
 function App() {
   return (
     <Router>
-      <UserProvider>
-        <Switch>
-          <div className="App">
-            <ProtectedRoute path="/" exact>
-              <Redirect to="/home" />
-            </ProtectedRoute>
-            <ProtectedRoute path="/home" exact>
-              <Redirect to="/home/milestones" />
-            </ProtectedRoute>
-            <ProtectedRoute path="/home/milestones">
-              <AppWrapper>
-                <Milestones />
-              </AppWrapper>
-            </ProtectedRoute>
-            <ProtectedRoute path="/home/beliefs">
-              <AppWrapper>
-                <Beliefs />
-              </AppWrapper>
-            </ProtectedRoute>
-            <ProtectedRoute path="/focus" exact>
-              <Redirect to="/focus/system" />
-            </ProtectedRoute>
-            <ProtectedRoute path="/focus/system">
-              <AppWrapper>
-                <System />
-              </AppWrapper>
-            </ProtectedRoute>
-            <ProtectedRoute path="/focus/swagger"></ProtectedRoute>
-            <ProtectedRoute path="/focus/sleep"></ProtectedRoute>
-            <ProtectedRoute path="/focus/habits"></ProtectedRoute>
+      <ErrorProvider>
+        <UserProvider>
+          <Switch>
+            <div className="App">
+              <ProtectedRoute path="/" exact>
+                <Redirect to="/home" />
+              </ProtectedRoute>
+              <ProtectedRoute path="/home" exact>
+                <Redirect to="/home/milestones" />
+              </ProtectedRoute>
+              <ProtectedRoute path="/home/milestones">
+                <AppWrapper>
+                  <Milestones />
+                </AppWrapper>
+              </ProtectedRoute>
+              <ProtectedRoute path="/home/beliefs">
+                <AppWrapper>
+                  <Beliefs />
+                </AppWrapper>
+              </ProtectedRoute>
+              <ProtectedRoute path="/focus" exact>
+                <Redirect to="/focus/system" />
+              </ProtectedRoute>
+              <ProtectedRoute path="/focus/system">
+                <AppWrapper>
+                  <System />
+                </AppWrapper>
+              </ProtectedRoute>
+              <ProtectedRoute path="/focus/swagger"></ProtectedRoute>
+              <ProtectedRoute path="/focus/sleep"></ProtectedRoute>
+              <ProtectedRoute path="/focus/habits"></ProtectedRoute>
 
-            <ProtectedRoute path="/fasting">
-              <Redirect to="/fasting/window" />
-            </ProtectedRoute>
-            <ProtectedRoute path="/food">
-              <Redirect to="/food/calories" />
-            </ProtectedRoute>
-            <ProtectedRoute path="/fitness">
-              <Redirect to="/fitness/activities" />
-            </ProtectedRoute>
+              <ProtectedRoute path="/fasting">
+                <Redirect to="/fasting/window" />
+              </ProtectedRoute>
+              <ProtectedRoute path="/food">
+                <Redirect to="/food/calories" />
+              </ProtectedRoute>
+              <ProtectedRoute path="/fitness">
+                <Redirect to="/fitness/activities" />
+              </ProtectedRoute>
 
-            <Route path="/login">
-              <LoginForm />
-            </Route>
-            <Route path="/signup">
-              <SignupForm />
-            </Route>
-          </div>
-        </Switch>
-      </UserProvider>
+              <Route path="/login">
+                <LoginForm />
+              </Route>
+              <Route path="/signup">
+                <SignupForm />
+              </Route>
+            </div>
+          </Switch>
+        </UserProvider>
+      </ErrorProvider>
     </Router>
   );
 }
---
+++ src/components/error/Error.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { ErrorContext } from "../../context/error/ErrorContext";
+// import { StyledNotificationPopup } from "../../styles/GlobalStyledComponents";
+
+const Error = () => {
+  const { errorMessage } = useContext(ErrorContext);
+
+  return (
+    // errorMessage !== null && (
+    //   <StyledNotificationPopup origin={"error"} draggable>
+    //     {errorMessage}
+    //   </StyledNotificationPopup>
+    // )
+  );
+};
+
+export default Error;
