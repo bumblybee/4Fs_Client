dca4edb32dc744fd3c31224af3e683fa393e4eef
Tiffani Hess <hesstjune@gmail.com>
2021-05-10 14:19:03 -0500

    Defined api calls for Sleep

--- src/api/focus/sleepApi.js
+++ src/api/focus/sleepApi.js
@@ -1,6 +1,17 @@
 import { get, post, put, destroy } from "../baseApi";
 
+export const getSleep = async () => {
+  const sleep = await get("/sleep");
+  return sleep.data;
+};
+
 export const mutateSleep = async (data, id) => {
   // Expects yy-mm-dd and 24:00
-  return await post(`/sleep/${id}`, data);
+  const sleep = await post(`/sleep/${id}`, data);
+  return sleep.data;
+};
+
+export const deleteSleep = async (id) => {
+  const deletedSleep = await destroy(`/sleep/${id}`);
+  return deletedSleep.data;
 };
