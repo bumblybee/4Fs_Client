409b61421ebf3763457f6c2a193977eff8120bf2
Tiffani Hess <hesstjune@gmail.com>
2021-05-13 16:40:11 -0500

    Brought user context into user profile, setting state with user data and pre-filling form

--- src/pages/user/UserProfile.js
+++ src/pages/user/UserProfile.js
@@ -1,31 +1,68 @@
-import React from "react";
-import { Container, Segment, Form, Button } from "semantic-ui-react";
+import React, { useContext, useState, useEffect } from "react";
+import { Segment, Form, Button } from "semantic-ui-react";
+import { UserContext } from "../../context/user/UserContext";
 
 const UserProfile = () => {
+  const { user } = useContext(UserContext);
+  const splitPhoneNumber = user && user.phone && user.phone.split("-");
+  const [userDetails, setUserDetails] = useState({
+    id: null,
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone1: "",
+    phone2: "",
+    phone3: "",
+    age: "",
+    height: null,
+    weight: null,
+    gender: "",
+    sheetsUrl: "",
+  });
+
+  useEffect(() => {
+    user &&
+      setUserDetails({
+        ...userDetails,
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phone1: splitPhoneNumber && splitPhoneNumber[0],
+        phone2: splitPhoneNumber && splitPhoneNumber[1],
+        phone3: splitPhoneNumber && splitPhoneNumber[2],
+        age: user.age,
+        height: user.height,
+        weight: user.weight,
+        gender: user.gender,
+        sheetsUrl: user.sheetsUrl,
+      });
+  }, [user]);
+
   return (
-    <Segment style={{ maxWidth: "70%", margin: "0 auto" }}>
-      <Segment
-        basic
-        style={{ maxWidth: "60%", margin: "0 auto", padding: "2rem" }}
-      >
+    <Segment style={{ maxWidth: "70%", margin: "5% auto" }} raised>
+      <Segment basic padded style={{ maxWidth: "65%", margin: "0 auto" }}>
         <h1 style={{ textAlign: "center", marginBottom: "2rem" }}>
           Member Profile
         </h1>
-        <Form>
+        <Form style={{ padding: "1rem" }}>
           <Form.Group widths="equal">
             <Form.Input
               fluid
               icon="user"
               iconPosition="left"
-              placeholder="First"
-              label="First name"
+              placeholder="First name"
+              label="First Name"
+              value={userDetails.firstName}
             />
+
             <Form.Input
               fluid
               icon="user"
               iconPosition="left"
-              placeholder="Last"
-              label="Last name"
+              placeholder="Last name"
+              label="Last Name"
+              value={userDetails.lastName}
             />
           </Form.Group>
           <Form.Input
@@ -35,22 +72,40 @@ const UserProfile = () => {
             placeholder="example@example.com"
             label="Email"
             type="email"
-          />{" "}
-          <Form.Input
-            fluid
-            icon="phone"
-            iconPosition="left"
-            placeholder="555-555-5555"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            label="Phone Number"
-            type="tel"
+            value={userDetails.email}
           />
+          <Form.Group inline widths="equal">
+            <Form.Input
+              fluid
+              icon="phone"
+              iconPosition="left"
+              placeholder="555"
+              label="Phone Number"
+              type="text"
+              value={userDetails.phone1}
+            />{" "}
+            <Form.Input
+              fluid
+              placeholder="555"
+              label=""
+              type="text"
+              value={userDetails.phone2}
+            />{" "}
+            <Form.Input
+              fluid
+              placeholder="5555"
+              label=" "
+              type="text"
+              value={userDetails.phone3}
+            />
+          </Form.Group>
           <Form.Input
             fluid
             icon="google drive"
             iconPosition="left"
             placeholder="https://docs.google.com/spreadsheets/d/CveaIsVeKQ"
             label="Google Sheets Link"
+            value={userDetails.sheetsUrl}
           />
           <Form.Group widths="equal">
             <Form.Input
@@ -62,6 +117,7 @@ const UserProfile = () => {
               placeholder="in"
               min="40"
               max="90"
+              value={userDetails.height}
             />
 
             <Form.Input
@@ -72,6 +128,7 @@ const UserProfile = () => {
               label="Weight"
               placeholder="lb"
               min="70"
+              value={userDetails.weight}
             />
           </Form.Group>
           <Form.Group widths="equal">
@@ -83,6 +140,7 @@ const UserProfile = () => {
               placeholder="yr"
               min="15"
               type="number"
+              value={userDetails.age}
             />
             <Form.Select
               fluid
@@ -93,6 +151,7 @@ const UserProfile = () => {
                 { key: "f", text: "Female", value: "female" },
                 { key: "o", text: "Other", value: "other" },
               ]}
+              value={userDetails.gender}
             />
           </Form.Group>
           <Button color="blue" fluid>
