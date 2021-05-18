f85ddb1210be51551f6fd124e5f12e9316299937
Tiffani Hess <hesstjune@gmail.com>
2021-05-18 15:18:51 -0500

    destructed week dates from priorPractices and map into accordian titles

--- src/components/focus/system/PriorPractices.js
+++ src/components/focus/system/PriorPractices.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useCRUD from "../../../hooks/useCRUD";
 import { getPriorPractices } from "../../../api/focus/practicesApi";
+import { Accordion, Icon } from "semantic-ui-react";
 
 const PriorPractices = () => {
   const [priorPractices] = useCRUD(getPriorPractices);
-  console.log(priorPractices);
-  return <div></div>;
+  const weeks = priorPractices.map((practice) => ({
+    startDate: practice.practice_week.startDate,
+    endDate: practice.practice_week.endDate,
+  }));
+
+  // Overflow removes border on accordian - style manually
+  return (
+    <div style={{ height: "140px", overflowY: "auto" }}>
+      <Accordion styled>
+        {weeks.map((week) => (
+          <Accordion.Title>
+            <Icon name="dropdown" />
+            {week.startDate}-{week.endDate}
+          </Accordion.Title>
+        ))}
+      </Accordion>
+    </div>
+  );
 };
 
 export default PriorPractices;
