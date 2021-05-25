7cef00e3d2fdf1c2892ff55d33be5869579cc35f
Tiffani Hess <hesstjune@gmail.com>
2021-05-24 21:02:36 -0500

    Defined habits api calls

--- src/api/focus/habitsApi.js
+++ src/api/focus/habitsApi.js
@@ -1,5 +1,16 @@
-import { get, post, put, destroy } from "../baseApi";
+import { get, post, destroy } from "../baseApi";
+
+export const getHabits = async () => {
+  const habits = await get("/habits");
+  return habits.data ? habits.data : habits;
+};
 
 export const mutateHabit = async (data, id) => {
-  return await post(`/habits/${id}`, data);
+  const habit = await post(`/habits/${id}`, data);
+  return habit.data ? habit.data : habit;
+};
+
+export const deleteHabit = async (id) => {
+  const habit = await destroy(`/habits/${id}`);
+  return habit.data ? habit.data : habit;
 };
