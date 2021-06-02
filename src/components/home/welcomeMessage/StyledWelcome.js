b4af01a0fad7a565c1bc0fdde51c6a823c672194
Tiffani Hess <hesstjune@gmail.com>
2021-06-02 15:49:06 -0500

    Welcome message styling and visible state. Option to close message but currently not using

--- src/components/home/welcomeMessage/StyledWelcome.js
+++ src/components/home/welcomeMessage/StyledWelcome.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 import { Segment, Message } from "semantic-ui-react";
 
-export const StyledWelcomeMessage = styled(Segment)`
+export const StyledWelcomeMessage = styled(Message)`
   width: 25% !important;
   margin: -1.5% auto 1% !important;
-
+  /* background: ${(props) => !props.visible && "#fff !important"};
+  box-shadow: ${(props) => !props.visible && "none !important"}; */
   text-align: justify;
+  position: relative;
+  cursor: pointer;
 
   .header {
     text-align: center;
+  }
+
+  i {
+    position: absolute;
+    top: ${(props) => (props.visible ? "5%" : "25%")};
+    right: 2%;
   }
 `;
--- src/components/home/welcomeMessage/welcomeMessage.js
+++ src/components/home/welcomeMessage/welcomeMessage.js
@@ -1,22 +1,51 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../../context/user/UserContext";
+import { Icon, Button, Message } from "semantic-ui-react";
 import { StyledWelcomeMessage } from "./StyledWelcome";
 
 const WelcomeMessage = () => {
   const { user } = useContext(UserContext);
-  const [name, setName] = useState();
-
-  useEffect(() => {
-    user && setName(user.firstName);
-  }, [user]);
+  const [visible, setVisible] = useState(true);
+  const list = [
+    "To come back to the home page, click the 4Fs logo in the top left corner",
+    "To access your profile, click the user icon in the top right corner",
+    "Use the large rectangular menu above to move through each of the four F's",
+    "Use the smaller menu directly above this message to access each section within the four F's",
+  ];
 
   return (
-    <StyledWelcomeMessage
-      header={`Welcome ${name}!`}
-      content="To get back to this page, click the 4Fs logo in the top left corner. To access your profile, click the user icon in the top right corner. Use the large rectangular menu above to move through each of the four F's. Use the smaller menu directly above this message to access each section within the four F's."
-      // size="small"
-      floating
-    ></StyledWelcomeMessage>
+    user && (
+      // <StyledWelcomeMessage
+      //   // content="To get back to this page, click the 4Fs logo in the top left corner. To access your profile, click the user icon in the top right corner. Use the large rectangular menu above to move through each of the four F's. Use the smaller menu directly above this message to access each section within the four F's."
+
+      //   // list={list}
+      //   visible={visible}
+      //   size="small"
+      //   floating
+      //   onClick={() => setVisible(!visible)}
+      // >
+      //   <Message.Header>
+      //     {`Welcome ${user.firstName}!`} <Icon name={visible ? "close" : ""} />
+      //   </Message.Header>
+      //   {visible && (
+      //     <Message.List>
+      //       {list.map((item) => (
+      //         <Message.Item>{item}</Message.Item>
+      //       ))}
+      //     </Message.List>
+      //   )}
+      // </StyledWelcomeMessage>
+
+      <StyledWelcomeMessage list={list} size="small" floating>
+        <Message.Header>{`Welcome ${user.firstName}!`}</Message.Header>
+
+        <Message.List>
+          {list.map((item) => (
+            <Message.Item>{item}</Message.Item>
+          ))}
+        </Message.List>
+      </StyledWelcomeMessage>
+    )
   );
 };
 
