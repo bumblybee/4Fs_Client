4f722a9b59d1c8df79c85b9813b66580394c858d
Tiffani Hess <hesstjune@gmail.com>
2021-06-03 13:20:02 -0500

    Decreased width of WelcomeMessage. Padding bottom on Contact

--- src/components/home/welcome/StyledWelcome.js
+++ src/components/home/welcome/StyledWelcome.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { Message } from "semantic-ui-react";
 
 export const StyledWelcomeMessage = styled(Message)`
-  width: 30% !important;
+  width: 28% !important;
   margin: -1.25% auto 0.65% !important;
   text-align: center;
   /* font-size: 1rem !important; */
--- src/components/layout/contact/StyledContact.js
+++ src/components/layout/contact/StyledContact.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledContact = styled.div`
   width: max-content;
   margin: 0 auto;
+  padding-bottom: 1rem;
   font-weight: 600;
   font-size: 1.1rem;
 `;
