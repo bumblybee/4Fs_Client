acbeb920319d831eb3ecfdb808cddb9f6edc61dd
Tiffani Hess <hesstjune@gmail.com>
2021-06-02 21:09:26 -0500

    Created NavBar stylesheet and styled buttons

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,60 +1,34 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
-import { Image, Button } from "semantic-ui-react";
+import { Image, Button, Icon } from "semantic-ui-react";
+import { StyledNav, StyledUserButton, StyledLogoButton } from "./StyledNavBar";
 
 const NavBar = () => {
   // Using history push when click user profile so forces full page reload to ensure user date loaded - maybe send state with link instead?
   const history = useHistory();
   return (
-    <div
-      style={{
-        padding: "1.25rem 2rem 1rem",
-        background:
-          "linear-gradient(135deg, rgba(105,165,232,1) -1%, rgba(49,103,158,1) 30%,rgba(40,124,202,1) 91%,rgba(115,165,232,1) 100%)",
-        display: "flex",
-        alignContent: "center",
-        width: "100%",
-      }}
-    >
-      <div style={{ marginRight: "auto" }}>
-        <Link to="/home">
-          <Button
-            size="medium"
-            title="home"
-            style={{
-              borderRadius: "50%",
-              padding: "8px 7px",
-              boxShadow: "0 0 10px 2px rgba(17,47,167,0.3)",
-              border: "2px solid #2f528b",
-              background:
-                "linear-gradient(135deg, rgba(255,255,255,1) 0%,rgba(246,246,246,1) 47%,rgba(237,237,237,1) 100%)",
-            }}
-          >
-            <Image style={{ height: "2.45rem" }} src="/4flogo.png" circular />
-          </Button>
-        </Link>
-      </div>
-
-      <div style={{ marginLeft: "auto" }}>
-        <Button
+    <StyledNav>
+      <div>
+        <StyledLogoButton
           as="a"
           size="huge"
-          icon="user"
-          title="profile"
-          style={{
-            borderRadius: "50%",
-            padding: "14px 14px",
-            boxShadow: "0 0 10px 2px rgba(17,47,167,0.3)",
-            color: "#2f528b",
-            border: "2px solid #2f528b",
-            marginRight: "0",
-            background:
-              "linear-gradient(135deg, rgba(255,255,255,1) 0%,rgba(246,246,246,1) 47%,rgba(237,237,237,1) 100%)",
-          }}
-          onClick={() => history.push("/profile")}
-        ></Button>
+          title="home"
+          onClick={() => history.push("/home")}
+        >
+          <Image src="/4flogo.png" circular />
+        </StyledLogoButton>
       </div>
-    </div>
+
+      <div>
+        <StyledUserButton
+          as="a"
+          title="profile"
+          onClick={() => history.push("/profile")}
+        >
+          <Icon name="user" size="large" />
+        </StyledUserButton>
+      </div>
+    </StyledNav>
   );
 };
 
---
+++ src/components/nav/StyledNavBar.js
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+import { Button } from "semantic-ui-react";
+
+export const StyledNav = styled.div`
+  padding: 1.25rem 2rem 1rem;
+  background: linear-gradient(
+    135deg,
+    rgba(105, 165, 232, 1) -1%,
+    rgba(49, 103, 158, 1) 30%,
+    rgba(40, 124, 202, 1) 91%,
+    rgba(115, 165, 232, 1) 100%
+  );
+  display: flex;
+  align-content: center;
+  width: 100%;
+
+  div:first-child {
+    margin-right: auto;
+  }
+
+  div:nth-child(2) {
+    margin-left: auto;
+  }
+`;
+
+export const StyledUserButton = styled(Button)`
+  border-radius: 50% !important;
+  padding: 13px 11px 13px 13px !important;
+  box-shadow: 0 0 10px 2px rgba(17, 47, 167, 0.3);
+  color: #2f528b;
+  border: 2px solid #2f528b;
+  margin-right: 0;
+  background: linear-gradient(
+    135deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(246, 246, 246, 1) 47%,
+    rgba(237, 237, 237, 1) 100%
+  );
+  display: flex !important;
+  align-content: center;
+  justify-content: center;
+  cursor: pointer;
+
+  i {
+    font-size: 1.6rem !important;
+  }
+
+  :hover {
+    background: linear-gradient(
+      135deg,
+      rgba(255, 255, 255, 0.9) 0%,
+      rgba(246, 246, 246, 0.9) 47%,
+      rgba(237, 237, 237, 0.9) 100%
+    );
+  }
+
+  :active {
+    transform: scale(0.99);
+    i {
+      font-size: 1.55rem !important;
+    }
+    box-shadow: 0 0 2px 1px rgba(17, 47, 167, 0.4);
+  }
+`;
+
+export const StyledLogoButton = styled(Button)`
+  border-radius: 50% !important;
+  padding: 8px 7px;
+  box-shadow: 0 0 10px 2px rgba(17, 47, 167, 0.3);
+  border: 2px solid #2f528b;
+  background: linear-gradient(
+    135deg,
+    rgba(255, 255, 255, 1) 0%,
+    rgba(246, 246, 246, 1) 47%,
+    rgba(237, 237, 237, 1) 100%
+  );
+  display: block !important;
+  cursor: pointer;
+
+  img {
+    height: 2.45rem;
+  }
+
+  :hover {
+    background: linear-gradient(
+      135deg,
+      rgba(255, 255, 255, 0.9) 0%,
+      rgba(246, 246, 246, 0.9) 47%,
+      rgba(237, 237, 237, 0.9) 100%
+    );
+  }
+
+  :active {
+    transform: scale(0.99);
+    box-shadow: 0 0 2px 1px rgba(17, 47, 167, 0.4);
+
+    img {
+      transform: scale(0.99);
+    }
+  }
+`;
