78c1bed5c933a2ba2815baa559f31797d316b59b
Tiffani Hess <hesstjune@gmail.com>
2021-06-04 17:06:29 -0500

    Moved Sleep inline styles to stylesheet

--- src/components/focus/sleep/SleepTableHeader.js
+++ src/components/focus/sleep/SleepTableHeader.js
@@ -1,18 +1,12 @@
 import React from "react";
 import CsvDownloader from "react-csv-downloader";
-
 import { Icon, Popup } from "semantic-ui-react";
+import { StyledSleepHeader } from "./StyledSleep";
 
 const SleepTableHeader = ({ data, columns }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        width: "100%",
-      }}
-    >
-      <div style={{ marginRight: "auto", cursor: "pointer" }}>
+    <StyledSleepHeader>
+      <div className="csv">
         <CsvDownloader
           datas={data}
           columns={columns}
@@ -26,18 +20,18 @@ const SleepTableHeader = ({ data, columns }) => {
           />
         </CsvDownloader>
       </div>
-      <div style={{ margin: "auto", fontStyle: "italic" }}>
+      <div className="heading">
         Pro Tip: Set a timer on your phone for your ideal bedtime and wake up
         time.
       </div>
-      <div style={{ marginLeft: "auto" }}>
+      <div className="info">
         <Popup
           position="top center"
           content="Hours slept will calculate automatically based on the time you went to bed and the time you woke up."
           trigger={<Icon name="question circle outline" />}
         />
       </div>
-    </div>
+    </StyledSleepHeader>
   );
 };
 
---
+++ src/components/focus/sleep/StyledSleep.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledSleepHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+
+  .csv {
+    margin-right: auto;
+    cursor: pointer;
+  }
+
+  .heading {
+    margin: auto;
+    font-style: italic;
+  }
+
+  .info {
+    margin-left: auto;
+  }
+`;
