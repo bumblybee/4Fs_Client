28428590608fef53891a82b403ce00884eacb06d
Tiffani Hess <hesstjune@gmail.com>
2021-06-07 15:59:23 -0500

    Width for all screen sizes - WelcomeMessage

--- src/components/home/welcome/StyledWelcome.js
+++ src/components/home/welcome/StyledWelcome.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import { Message } from "semantic-ui-react";
 
 export const StyledWelcomeMessage = styled(Message)`
-  width: 28% !important;
-  margin: -1.25% auto 0.65% !important;
+  width: 38% !important;
+  margin: -1.25% auto 1% !important;
   text-align: center;
   padding: 1.5rem !important;
 
@@ -12,8 +12,18 @@ export const StyledWelcomeMessage = styled(Message)`
     font-size: 1.1rem !important;
   }
 
+  @media (max-width: 1024px) {
+    width: 74% !important;
+    margin: -1% auto 2% !important;
+  }
+
+  @media (max-width: 768px) {
+    width: 80% !important;
+    margin: -1% auto 2% !important;
+  }
+
   @media (max-width: 620px) {
-    width: 90% !important;
+    width: 93% !important;
     margin: -5% auto 5% !important;
   }
 `;
