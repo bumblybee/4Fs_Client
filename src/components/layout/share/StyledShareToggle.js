5703b7060272b899a7ef0230cded7cef2c5d3639
Tiffani Hess <hesstjune@gmail.com>
2021-06-07 21:42:26 -0500

    Adusted ShareToggle margin

--- src/components/layout/share/StyledShareToggle.js
+++ src/components/layout/share/StyledShareToggle.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledToggle = styled.div`
   width: ${(props) => props.width};
-  margin: ${(props) => (props.width ? "0 auto 0.5rem" : "0 0 0.5rem 0.25rem")};
+  margin: ${(props) => (props.width ? "0 auto 0.5rem" : "0 0 0.5rem 0.5rem")};
 
   label {
     font-weight: 600;
