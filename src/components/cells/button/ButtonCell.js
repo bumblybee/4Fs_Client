3a545483548d3b218d217e8cd2adcdfd938de7b4
Tiffani Hess <hesstjune@gmail.com>
2021-06-09 09:17:30 -0500

    Brought function to get window size into ButtonCell and render size based on viewport. Deleted StyledButtonCell

--- src/components/cells/button/ButtonCell.js
+++ src/components/cells/button/ButtonCell.js
@@ -1,22 +1,37 @@
-import React from "react";
-import { StyledButton } from "./StyledButtonCell";
+import React, { useState, useEffect } from "react";
+import { Button } from "semantic-ui-react";
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return { width, height };
+};
 
 const ButtonCell = (props) => {
+  const [windowSize, setWindowSize] = useState();
+
+  useEffect(() => {
+    window.addEventListener("resize", setWindowSize(getWindowDimensions));
+
+    return () => {
+      window.removeEventListener("resize", setWindowSize(getWindowDimensions));
+    };
+  }, []);
+
   return (
-    <StyledButton
+    <Button
       as="a"
       href={props.to}
       target={props.newWindow && "_blank"}
       rel="noopener"
       accessor={props.accessor}
       color={props.color}
+      size={windowSize && windowSize.width > 385 ? props.size : "large"}
       isCentered={props.isCentered}
-      className={`ui ${props.color} ${props.size} button`}
       newWindow={props.newWindow}
     >
       {props.icon && props.icon}
       {props.val}
-    </StyledButton>
+    </Button>
   );
 };
 
