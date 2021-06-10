d722683d034aab96dd80606e5acaec16ca963838
Tiffani Hess <hesstjune@gmail.com>
2021-06-10 18:18:09 -0500

    Remove check for val at ButtonCell before rendering JSX

--- src/components/cells/button/ButtonCell.js
+++ src/components/cells/button/ButtonCell.js
@@ -4,24 +4,22 @@ import { StyledButtonCell } from "./StyledButtonCell";
 
 const ButtonCell = (props) => {
   return (
-    props.val && (
-      <StyledButtonCell>
-        <Button
-          as="a"
-          href={props.to}
-          target={props.newWindow && "_blank"}
-          rel="noopener"
-          accessor={props.accessor}
-          color={props.color}
-          isCentered={props.isCentered}
-          newWindow={props.newWindow}
-          size={props.size || "large"}
-        >
-          {props.icon && props.icon}
-          {props.val || "Add Google Sheets Link"}
-        </Button>
-      </StyledButtonCell>
-    )
+    <StyledButtonCell>
+      <Button
+        as="a"
+        href={props.to}
+        target={props.newWindow && "_blank"}
+        rel="noopener"
+        accessor={props.accessor}
+        color={props.color}
+        isCentered={props.isCentered}
+        newWindow={props.newWindow}
+        size={props.size || "large"}
+      >
+        {props.icon && props.icon}
+        {props.val || "Add Google Sheets Link"}
+      </Button>
+    </StyledButtonCell>
   );
 };
 
