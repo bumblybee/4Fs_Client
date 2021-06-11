1e58c94efdd3aefd342ffdbd3c23d53709a8606a
Tiffani Hess <hesstjune@gmail.com>
2021-06-10 19:16:08 -0500

    Open contact link in new window, adjust SectionContainer styling so contact link a bit lower

--- src/components/layout/StyledSectionContainer.js
+++ src/components/layout/StyledSectionContainer.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledSectionContainer = styled.div`
-  padding-bottom: 1rem;
+  padding-bottom: 1.5rem;
 
   > div {
     height: inherit;
--- src/components/layout/contact/Contact.js
+++ src/components/layout/contact/Contact.js
@@ -4,7 +4,9 @@ import { StyledContact } from "./StyledContact";
 const Contact = () => {
   return (
     <StyledContact>
-      <a href="mailto:hello@4fsofweightloss.com">Contact us</a>
+      <a href="mailto:hello@4fsofweightloss.com" target="_blank" rel="noopener">
+        Contact us
+      </a>
     </StyledContact>
   );
 };
