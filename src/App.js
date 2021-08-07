589722a1c1cb61c56880249bddc380e462cc8b33
Tiffani Hess <hesstjune@gmail.com>
2021-08-07 16:57:21 -0500

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import { Router } from "react-router-dom";
-import { history } from "./utils/customHistory";
-import CellProvider from "./context/cell/CellProvider";
-import NotificationProvider from "./context/notification/NotificationProvider";
-import Notification from "./components/notification/Notification";
-import Routes from "./components/routes";
+
 import Maintenance from "./pages/maintenance/Maintenance";
 
 function App() {
